056ab389da2aacb0e011dce71424e8cfa2b700e6
WillyD <sopwill3@gmail.com>
2020-01-06 16:15:21 -0700

    Added flow connect and reorganized a bit

--- src/components/ProjectsPage/Project1.jsx
+++ src/components/ProjectsPage/Project1.jsx
@@ -5,9 +5,10 @@ import ProjectExample from '../ProjectExample/ProjectExample.jsx';
 
 // Text
 import text from '../../text/text.js'
+
+// Images
 import holdem from '../../images/holdemlogo.jpg'
 import emphasis from '../../images/EmphasisDBLogo.png'
-import dance from '../../images/dance.png'
 
 function Project1(props) {
 
@@ -19,23 +20,16 @@ function Project1(props) {
     >
 
       <ProjectExample
-        postion = {{top: "13vh", left: "10vw"}}
+        postion = {{top: "13vh", left: "20vw"}}
         imageUrl = {holdem}
         projectLink = "https://holdem-io.herokuapp.com/"
         projectDescription = "Online mulitplayer poker built with React.js and Socket.io"
         projectName = "Holdem.io"
       />
 
-      <ProjectExample
-        postion = {{top: "15vh", left: "40vw"}}
-        imageUrl = {dance}
-        projectLink = "https://willyd332.github.io/Dance-Dance/"
-        projectDescription = "An HTML canvas music game"
-        projectName = "Dance Dance"
-      />
 
       <ProjectExample
-        postion = {{top: "55vh", left: "55vw"}}
+        postion = {{top: "55vh", left: "50vw"}}
         imageUrl = {emphasis}
         projectLink = "http://emphasisdb.herokuapp.com/"
         projectDescription = "Online Database for the Emphasis AI NLP model"
--- src/components/ProjectsPage/Project2.jsx
+++ src/components/ProjectsPage/Project2.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 // Components
 import ProjectExample from '../ProjectExample/ProjectExample.jsx';
 
+
+// Images
+import flow from '../../images/flow.png'
+import dance from '../../images/dance.png'
+
 function Project2(props) {
 
   return (
@@ -11,6 +16,25 @@ function Project2(props) {
       className="projectBox"
       style={props.style}
     >
+
+
+      <ProjectExample
+        postion = {{top: "10vh", left: "65vw"}}
+        imageUrl = {flow}
+        projectLink = "https://apps.apple.com/us/app/flow-connect-lite/id1471919193"
+        projectDescription = "iOS/React app that helps you manage personal connections"
+        projectName = "Flow Connect"
+      />
+
+
+      <ProjectExample
+        postion = {{top: "50vh", left: "40vw"}}
+        imageUrl = {dance}
+        projectLink = "https://willyd332.github.io/Dance-Dance/"
+        projectDescription = "An HTML canvas music game"
+        projectName = "Dance Dance"
+      />
+
 
     </div>
   );
