9d7263a6e738481740d3b23d120eafdb42e0021d
WillyD <sopwill3@gmail.com>
2020-01-08 15:43:46 -0700

    Updates

---
+++ precache-manifest.626d83b78a4bc26ddd2d9492b118c144.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f954155d222a784c4eb1a909f109a2",
+    "url": "/PORTFOLIO_1/index.html"
+  },
+  {
+    "revision": "299fe7861a34ed43927e",
+    "url": "/PORTFOLIO_1/static/css/main.59126f55.chunk.css"
+  },
+  {
+    "revision": "9da9e0d08f31506faf57",
+    "url": "/PORTFOLIO_1/static/js/2.8fd6e034.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PORTFOLIO_1/static/js/2.8fd6e034.chunk.js.LICENSE"
+  },
+  {
+    "revision": "299fe7861a34ed43927e",
+    "url": "/PORTFOLIO_1/static/js/main.c949f81c.chunk.js"
+  },
+  {
+    "revision": "523a21c155ad839eee16",
+    "url": "/PORTFOLIO_1/static/js/runtime-main.df0e55be.js"
+  },
+  {
+    "revision": "92064cc1be28f8615f45deac857dfe0b",
+    "url": "/PORTFOLIO_1/static/media/EmphasisDBLogo.92064cc1.png"
+  },
+  {
+    "revision": "1be1436b74090dcdc1ab2a3acb259ef9",
+    "url": "/PORTFOLIO_1/static/media/TBD.1be1436b.jpg"
+  },
+  {
+    "revision": "88a287bebb3c11390cc58a95ccb5e6ae",
+    "url": "/PORTFOLIO_1/static/media/dance.88a287be.png"
+  },
+  {
+    "revision": "2e20ba66590820ea3689027e2939ace4",
+    "url": "/PORTFOLIO_1/static/media/flow.2e20ba66.png"
+  },
+  {
+    "revision": "2d294fb32176666a2f7de7a2ca8d2a25",
+    "url": "/PORTFOLIO_1/static/media/holdemlogo.2d294fb3.jpg"
+  },
+  {
+    "revision": "506323991b9dbd15e16fb3fdab2eed6b",
+    "url": "/PORTFOLIO_1/static/media/project1.50632399.jpg"
+  },
+  {
+    "revision": "cee8316920114c98a8d7e2c1ef6306ee",
+    "url": "/PORTFOLIO_1/static/media/project2.cee83169.jpg"
+  },
+  {
+    "revision": "53c611eb38fda535634b2b1ba624406f",
+    "url": "/PORTFOLIO_1/static/media/project3.53c611eb.jpg"
+  },
+  {
+    "revision": "a4df3784a308d20a40f74ba8074e0f1a",
+    "url": "/PORTFOLIO_1/static/media/project4.a4df3784.jpg"
+  },
+  {
+    "revision": "9547cc79873f76069477d87318921a58",
+    "url": "/PORTFOLIO_1/static/media/project5.9547cc79.jpg"
+  },
+  {
+    "revision": "5e272012d5868842383e9c44b54d6be0",
+    "url": "/PORTFOLIO_1/static/media/slide1.5e272012.jpg"
+  },
+  {
+    "revision": "754dbb326452d2699c8051bdc03ceebe",
+    "url": "/PORTFOLIO_1/static/media/slide2.754dbb32.jpg"
+  },
+  {
+    "revision": "0d85cb33267dca08f5c20fabc851fd93",
+    "url": "/PORTFOLIO_1/static/media/slide3.0d85cb33.jpg"
+  },
+  {
+    "revision": "c7ee81a9370ef0f1c141a5f4b5e3e610",
+    "url": "/PORTFOLIO_1/static/media/slide4.c7ee81a9.jpg"
+  },
+  {
+    "revision": "40bca6f32c5f77ac0d1b26942da24191",
+    "url": "/PORTFOLIO_1/static/media/slide5.40bca6f3.jpg"
+  },
+  {
+    "revision": "054765789027391e472355ddc05dc899",
+    "url": "/PORTFOLIO_1/static/media/vigenere.05476578.png"
+  }
+]);
